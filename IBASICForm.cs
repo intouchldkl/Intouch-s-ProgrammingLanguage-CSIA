3141842b4faf8da884df4945003555fde0fee464
inn@ascot.ac.th <inn@ascot.ac.th>
2021-07-19 21:52:43 +0700

    Finish TarrayDeclaration()

--- IBASICForm.cs
+++ IBASICForm.cs
@@ -101,7 +101,7 @@ namespace CS_IA_Ibasic_Intouch_Re
         private void Run_Click(object sender, EventArgs e)
         {
             IBASICtranslator translator = new IBASICtranslator(currentRtb.Lines);
-           currentRtb.Text =  translator.translateOUTPUT();
+           currentRtb.Text =  translator.TarrayDeclaration();
             Compiler Icompiler = new Compiler(currentRtb.Text);
             Icompiler.launchEXE();
            
--- IBASICtranslator.cs
+++ IBASICtranslator.cs
@@ -99,30 +99,63 @@ namespace CS_IA_Ibasic_Intouch_Re
                 line = IBASICcode[i].Trim();
                 keyword = line.Substring(0, 7);
                 string arraycheck = string.Concat(IBASICcode[i].Where(c => !Char.IsWhiteSpace(c)));
-                ///This if statement makes sure "INPUT" is not part of a variable name and its not array declaration
+                ///This if statement makes sure "DECLARE" is not part of a variable name and its not array declaration
                 if (line.Substring(7, 1) == " " &&
                     arraycheck.Contains("ARRAY[") == false)
                 {
                     string therest = line.Substring(7);
                     therest = therest.Trim();
                     string[] variable = therest.Split(':');
-                    variable[0] = variable[0].Trim();
+                    string variableName = variable[0].Trim();
                     variable[1] = variable[1].Trim();
                     string type1 = variable[1].Substring(0, 1).ToUpper();
                     string type2 = variable[1].Substring(1).ToLower();
                     if (keyword == "DECLARE")
                     {
-                        IBASICcode[i] = "Dim " + variable[0] + " As " + type1 + type2 ;
-                    
+                        IBASICcode[i] = "Dim " + variableName + " As " + type1 + type2;
+
                     }
                 }
             }
+        }
 
-            public string Tassignvalue()
+            public string TarrayDeclaration()
             {
-
-            }
-           
+                string line = "";
+                string keyword;
+                for (int i = 0; i < IBASICcode.Length; i++)
+                {
+                    line = IBASICcode[i].Trim();
+                    keyword = line.Substring(0, 7);
+                    ///get rid of all the spaces
+                    string arraycheck = string.Concat(IBASICcode[i].Where(c => !Char.IsWhiteSpace(c)));
+                    ///This if statement makes sure "DECLARE" is not part of a variable name and isa array declaration
+                    if (line.Substring(7, 1) == " " &&
+                        arraycheck.Contains("ARRAY[") == true)
+                    {
+                        string therest = line.Substring(7);
+                        therest = therest.Trim();
+                        string[] variablearray = therest.Split(':', 2);
+                        string variable = variablearray[0].Trim();
+                        string[] indexandtype = variablearray[1].Split(']');
+                        string type = indexandtype[1].Trim();
+                        string[] types = type.Split('F');
+                        types[1] = types[1].Trim();
+                        string type1 = types[1].Substring(0, 1).ToUpper();
+                        string type2 = types[1].Substring(1).ToLower();
+                        ///get rid of all the space to make it easier to separate indices
+                        indexandtype[0] = string.Concat(indexandtype[0].Where(c => !Char.IsWhiteSpace(c)));
+                        string[] Indices = indexandtype[0].Split(':');
+                        int index = int.Parse(Indices[1]);
+                        if (keyword == "DECLARE")
+                        {
+                            IBASICcode[i] = "Dim " + variable+"(" + (index-1) + ")" + " As " + type1 + type2;
+                            line = IBASICcode[i];
+                        }
+                    }
+                   
+                }
+            return line;
         }
         
     }
